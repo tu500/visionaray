39cbc2d436a29a2f6a4a12af568ff7adfd958fc1
Philip Matura <philip.matura@gmx.de>
2016-02-03 20:54:40 +0100

    Add TBB support and configuration for viewer

--- src/viewer/viewer.cpp
+++ src/viewer/viewer.cpp
@@ -45,7 +45,7 @@
 #include <visionaray/point_light.h>
 #include <visionaray/scheduler.h>
 
-#if defined(__MINGW32__) || defined(__MINGW64__)
+#if USE_TBB
 #include <visionaray/experimental/tbb_sched.h>
 #endif
 
@@ -192,7 +192,7 @@ struct renderer : viewer_type
     thrust::device_vector<material_type>    device_materials;
 #endif
 
-#if defined(__MINGW32__) || defined(__MINGW64__)
+#if USE_TBB
     tbb_sched<ray_type_cpu>     host_sched;
 #else
     tiled_sched<ray_type_cpu>   host_sched;
