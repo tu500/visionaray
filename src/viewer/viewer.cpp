e33e8ffbf501c6dd549820f5bbeb87d5c5b5c649
Philip Matura <philip.matura@gmx.de>
2016-03-23 18:55:50 +0100

    Precompiled header hack, POC

--- src/viewer/viewer.cpp
+++ src/viewer/viewer.cpp
@@ -1,70 +1,7 @@
 // This file is distributed under the MIT license.
 // See the LICENSE file for details.
 
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <istream>
-#include <ostream>
-#include <memory>
-#include <sstream>
-#include <string>
-#include <thread>
-
-#include <GL/glew.h>
-
-#include <visionaray/detail/platform.h>
-
-#if defined(VSNRAY_OS_DARWIN)
-
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9
-
-    #pragma GCC diagnostic ignored "-Wdeprecated"
-    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
-#endif
-
-#include <OpenGL/gl.h>
-#include <GLUT/glut.h>
-
-#else // VSNRAY_OS_DARWIN
-
-#include <GL/gl.h>
-#include <GL/glut.h>
-
-#endif
-
-#include <Support/CmdLine.h>
-#include <Support/CmdLineUtil.h>
-
-#include <visionaray/gl/debug_callback.h>
-#include <visionaray/texture/texture.h>
-#include <visionaray/aligned_vector.h>
-#include <visionaray/bvh.h>
-#include <visionaray/cpu_buffer_rt.h>
-#include <visionaray/kernels.h>
-#include <visionaray/point_light.h>
-#include <visionaray/scheduler.h>
-
-#if defined(__MINGW32__) || defined(__MINGW64__)
-#include <visionaray/experimental/tbb_sched.h>
-#endif
-
-#ifdef __CUDACC__
-#include <visionaray/gpu_buffer_rt.h>
-#include <visionaray/pixel_unpack_buffer_rt.h>
-#endif
-
-#include <common/manip/arcball_manipulator.h>
-#include <common/manip/pan_manipulator.h>
-#include <common/manip/zoom_manipulator.h>
-#include <common/call_kernel.h>
-#include <common/model.h>
-#include <common/obj_loader.h>
-#include <common/render_bvh.h>
-#include <common/timer.h>
-#include <common/util.h>
-#include <common/viewer_glut.h>
+#include "precompiled.h"
 
 
 using namespace visionaray;
